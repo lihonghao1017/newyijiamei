f22d7c04649e0fa1b3805df1a41d5f67a7bc5158
lihh <lihh@mapbar.com>
2018-11-08 15:10:36 +0800

    chg 蓝牙自动配对功能

--- src/com/sucetech/yijiamei/newui/BluthHopitalView.java
+++ src/com/sucetech/yijiamei/newui/BluthHopitalView.java
@@ -2,8 +2,10 @@ package com.sucetech.yijiamei.newui;
 
 import android.bluetooth.BluetoothAdapter;
 import android.bluetooth.BluetoothDevice;
+import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.content.IntentFilter;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.Message;
@@ -31,6 +33,8 @@ import com.sucetech.yijiamei.view.Bluetooth_Scale;
 import com.sucetech.yijiamei.view.ConBluthView;
 import com.zxing.activity.CaptureActivity;
 
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Set;
@@ -52,12 +56,16 @@ public class BluthHopitalView extends BaseView implements View.OnClickListener, 
     private Handler mHandler;
     private ConBluthView.SCALENOW scalenow = new ConBluthView.SCALENOW();
     private ListView boluthList;
+    private View boluthListLayout, boluthListUpdataLayout;
     private List<BluetoothDevice> deviceList;
     private String bluthMac;
+    private BluthAdapter bluthAdapter;
+    private BluetoothReceiver mBluetoothReceiver;
 
     public BluthHopitalView(Context context, AttributeSet attrs) {
         super(context, attrs);
     }
+
     public WuiaoYiyuanBean wuiaoYiyuanBean;
 
     @Override
@@ -68,7 +76,7 @@ public class BluthHopitalView extends BaseView implements View.OnClickListener, 
                     if (UserMsg.getMac() != null && !UserMsg.getMac().equals("")) {
                         startBlouth(UserMsg.getMac());
                     }
-                    wuiaoYiyuanBean= (WuiaoYiyuanBean) obj;
+                    wuiaoYiyuanBean = (WuiaoYiyuanBean) obj;
                     this.setVisibility(View.VISIBLE);
                     yiyuanData.addAll(wuiaoYiyuanBean.yiyaunBeanList);
                     hospitalAdapter.notifyDataSetChanged();
@@ -98,6 +106,9 @@ public class BluthHopitalView extends BaseView implements View.OnClickListener, 
         weightStr.setOnClickListener(this);
         boluthList = v.findViewById(R.id.boluthList);
         boluthList.setOnItemClickListener(this);
+        boluthListLayout = v.findViewById(R.id.boluthListLayout);
+        boluthListUpdataLayout = v.findViewById(R.id.boluthListUpdataLayout);
+        boluthListUpdataLayout.setOnClickListener(this);
         mHandler = new Handler() {
             public void handleMessage(Message msg) {
                 switch (msg.what) {
@@ -106,7 +117,8 @@ public class BluthHopitalView extends BaseView implements View.OnClickListener, 
                         ((MainActivity) getContext()).hideProgressDailogView();
                         Bundle bundle = msg.getData();
                         GetWeight(bundle.getByteArray("weight"));
-                        if (scalenow.bOverFlag) {;
+                        if (scalenow.bOverFlag) {
+                            ;
                         } else {
                             String wei = scalenow.sformatNetWeight.trim();
                             if (wei.contains("+")) {
@@ -114,11 +126,11 @@ public class BluthHopitalView extends BaseView implements View.OnClickListener, 
                             }
                             wei = wei.trim();
 
-                            weightStr.setText(wei+" KG");
+                            weightStr.setText(wei + " KG");
                             mEventManager.notifyObservers(EventStatus.weight, wei);
                         }
                         weightStr.setVisibility(View.VISIBLE);
-                        boluthList.setVisibility(View.GONE);
+                        boluthListLayout.setVisibility(View.GONE);
                         break;
                     case 2:
                         if (msg.arg1 == 0) {
@@ -134,7 +146,7 @@ public class BluthHopitalView extends BaseView implements View.OnClickListener, 
                         ((MainActivity) getContext()).hideProgressDailogView();
 
                         weightStr.setVisibility(View.GONE);
-                        boluthList.setVisibility(View.VISIBLE);
+                        boluthListLayout.setVisibility(View.VISIBLE);
 //                        if (msg.arg1 < 2) {
 //                            weidthStr.setText("----");
 //                            b_scaleIsConnect = Boolean.FALSE;
@@ -154,13 +166,21 @@ public class BluthHopitalView extends BaseView implements View.OnClickListener, 
     }
 
     private void initBluth() {
+
+        deviceList = new ArrayList<>();
+        bluthAdapter = new BluthAdapter(getContext(), deviceList);
+        boluthList.setAdapter(bluthAdapter);
+        initBluthBoundData();
+    }
+
+    private void initBluthBoundData() {
+        deviceList.clear();
         Set<BluetoothDevice> devices = BluetoothAdapter.getDefaultAdapter().getBondedDevices();
         if (devices.size() > 0) {
-            deviceList = new ArrayList<>();
             for (BluetoothDevice device : devices) {
                 deviceList.add(device);
             }
-            boluthList.setAdapter(new BluthAdapter(getContext(), deviceList));
+            bluthAdapter.notifyDataSetChanged();
         }
     }
 
@@ -174,11 +194,14 @@ public class BluthHopitalView extends BaseView implements View.OnClickListener, 
             case R.id.bluthLayout:
                 bluthIcon.setSelected(!bluthIcon.isSelected());
                 if (bluthIcon.isSelected()) {
-                    boluthList.setVisibility(View.VISIBLE);
+                    boluthListLayout.setVisibility(View.VISIBLE);
                 } else {
-                    boluthList.setVisibility(View.GONE);
+                    boluthListLayout.setVisibility(View.GONE);
                 }
 
+                break;
+            case R.id.boluthListUpdataLayout:
+                findBluthDevice();
                 break;
 
         }
@@ -188,13 +211,27 @@ public class BluthHopitalView extends BaseView implements View.OnClickListener, 
     public void onDestroy() {
         if (mBl_Scale != null) mBl_Scale.stop();
         System.exit(0);
+        if (mBluetoothReceiver != null) {
+            this.getContext().unregisterReceiver(mBluetoothReceiver);
+        }
     }
 
     @Override
     public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
         if (adapterView.getId() == R.id.boluthList) {
-            String mac = deviceList.get(i).getAddress();
-            startBlouth(mac);
+            BluetoothDevice device = deviceList.get(i);
+            if (device.getBondState() == BluetoothDevice.BOND_BONDED) {
+                String mac = deviceList.get(i).getAddress();
+                startBlouth(mac);
+            } else {
+                try {
+                    createBond(BluetoothDevice.class, device);
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+                Toast.makeText(getContext(), "kkkk", Toast.LENGTH_SHORT).show();
+            }
+
         } else {
             for (yiyaunBean bean : yiyuanData) {
                 bean.isSeleted = false;
@@ -230,12 +267,12 @@ public class BluthHopitalView extends BaseView implements View.OnClickListener, 
     }
 
     private void connectBluth(String bluth) {
-        bluthMac=bluth;
+        bluthMac = bluth;
         if (mBluetoothAdapter.enable()) {
             try {
                 device = mBluetoothAdapter.getRemoteDevice(bluth);
                 if (device != null) {
-                    if(!mBl_Scale.connect(device)){
+                    if (!mBl_Scale.connect(device)) {
                         Message message = Message.obtain();
                         message.what = 3;
                         message.obj = bluth;
@@ -310,6 +347,73 @@ public class BluthHopitalView extends BaseView implements View.OnClickListener, 
         public boolean bWeiStaFlag;
         public boolean bZeroFlag;
         public boolean bOverFlag;
+    }
+
+    private void findBluthDevice() {
+        if (mBluetoothReceiver == null) registerBluthBroadCast();
+        mBluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
+        initBluthBoundData();
+        if (mBluetoothAdapter.enable()) {
+            mBluetoothAdapter.startDiscovery();
+        }
+
+    }
+
+    private void registerBluthBroadCast() {
+        mBluetoothReceiver = new BluetoothReceiver();
+// 动态注册注册广播接收器。接收蓝牙发现讯息
+        IntentFilter btFilter = new IntentFilter();
+        btFilter.setPriority(1000);
+        btFilter.addAction(BluetoothDevice.ACTION_FOUND);
+        btFilter.addAction(BluetoothDevice.ACTION_PAIRING_REQUEST);
+        this.getContext().registerReceiver(mBluetoothReceiver, btFilter);
+    }
+
+    private boolean createBond(Class btClass, BluetoothDevice btDevice)
+            throws Exception {
+        Method createBondMethod = btClass.getMethod("createBond");
+        Boolean returnValue = (Boolean) createBondMethod.invoke(btDevice);
+        return returnValue.booleanValue();
+    }
+
+    class BluetoothReceiver extends BroadcastReceiver {
+
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            String action = intent.getAction(); //得到action
+            Log.e("action1=", action);
+            if (BluetoothDevice.ACTION_FOUND.equals(action)) {  //发现设备
+                BluetoothDevice btDevice = intent.getParcelableExtra(BluetoothDevice.EXTRA_DEVICE);
+                deviceList.add(btDevice);
+                bluthAdapter.notifyDataSetChanged();
+            }else if(BluetoothDevice.ACTION_CLASS_CHANGED .equals(action)){
+                Toast.makeText(getContext(), "ACTION_CLASS_CHANGED", Toast.LENGTH_SHORT).show();
+                BluetoothDevice btDevice = intent.getParcelableExtra(BluetoothDevice.EXTRA_DEVICE);
+                startBlouth(btDevice.getAddress());
+            }else if(BluetoothDevice.ACTION_PAIRING_REQUEST.equals(action)){
+                BluetoothDevice btDevice = intent.getParcelableExtra(BluetoothDevice.EXTRA_DEVICE);
+                try {
+                    Method setPairingConfirmation = btDevice.getClass().getDeclaredMethod("setPairingConfirmation",boolean.class);
+                    setPairingConfirmation.invoke(btDevice,true);
+                    abortBroadcast();//如果没有将广播终止，则会出现一个一闪而过的配对框。
+                    Method removeBondMethod = btDevice.getClass().getDeclaredMethod("setPin",
+                            new Class[]
+                                    {byte[].class});
+                    Boolean returnValue = (Boolean) removeBondMethod.invoke(btDevice,
+                            new Object[]
+                                    {"1234".getBytes()});
+                    Log.e("returnValue", "" + returnValue);
+
+                } catch (NoSuchMethodException e) {
+                    e.printStackTrace();
+                } catch (IllegalAccessException e) {
+                    e.printStackTrace();
+                } catch (InvocationTargetException e) {
+                    e.printStackTrace();
+                }
+
+            }
+        }
     }
 
 }
